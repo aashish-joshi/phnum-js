dc03a60e5d9e0454fd765feb1be9f29ea461eaa6
aashish-joshi <14803874+aashish-joshi@users.noreply.github.com>
2022-09-13 13:47:59 +0530

    cache validity 1hr

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,6 @@ export const KEY_USER_COUNTRY = 'sbn_userCountry';
 export const KEY_DISP_NUM = 'sbn_displayNum';
 export const KEY_DISP_NUM_COUNTRY = 'sbn_displayNumCountry';
 export const KEY_EXPIRY = 'sbn_expiry';
-export const CACHE_EXPIRY_TIME = 86400000;
+export const CACHE_EXPIRY_TIME = 3600000;
 export const VERSION = '0.3.0'
 export const DISPLAY_ELEMENT = '#show-best-num'
