8d9648c97ef11cb9bffc0476fe0e5387383b2296
aashish-joshi <14803874+aashish-joshi@users.noreply.github.com>
2022-11-11 23:43:30 +0530

    update docs
    add page for preparing JSON file.

---
+++ docs/index.js
@@ -0,0 +1,20 @@
+window.onload = () => {
+
+    const formJson = document.getElementById('create-json');
+    const copyButton = document.getElementById('copy-clipboard');
+    const textArea = document.getElementById('json');
+
+    formJson.addEventListener('submit',(e) => {
+        e.preventDefault();
+        const number = e.target.number.value;
+        console.log(number);
+        textArea.value += `${number},`;
+    });
+
+    copyButton.addEventListener('click',(e) => {
+        if(textArea.value){
+            navigator.clipboard.writeText(textArea.value);
+        }
+    });
+
+}
